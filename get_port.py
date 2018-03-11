850e18010ee3a20a6221e572276d200804e19ecd
MultipleCrashes <inimitableharish@gmail.com>
2018-03-11 06:48:41 +0530

    delete port req added
    Change-Id: I0bc6ddab89917c9e7824325ab19aca83e269e231

--- get_port.py
+++ get_port.py
@@ -5,13 +5,17 @@ import json
 host='localhost'
 port=9696
 
-query_string = 'http://' + host + ':' + str(port) + '/v2.0/ports'
-print('query string', query_string)
-all_ports = requests.get(query_string)
+get_query_string = 'http://' + host + ':' + str(port) + '/v2.0/ports'
+print('get query string', get_query_string)
+all_ports = requests.get(get_query_string)
 ports_json = json.loads(all_ports.text)
 port_list = ports_json['ports']
 for x in port_list:
     print '-'*20
-    print x
+    # For list of ports , issue a delete request
+    print 'deleting ', x['id']
+    delete_port_query_string = 'http://' + host + ':' + str(port) +'/v2.0/ports/' + str(x['id'])
+    delete_request = requests.delete(delete_port_query_string)
+    print 'Delete response ->', delete_request.text
 
 #pprint.pprint(all_ports.text)
