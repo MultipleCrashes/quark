dc903402ed392bc6ddc8a973ab98b36300168b88
MultipleCrashes <inimitableharish@gmail.com>
2018-03-11 07:20:06 +0530

    thread added
    Change-Id: I3a96b650b7321dbc1579cc4d291795339658e04d

--- create_ports.sh
+++ create_ports.sh
@@ -1,7 +1,7 @@
 host="localhost"
 port=9696
 
-for number in {1..100}
+for number in {1..1000}
 do
 curl -X POST -H "Content-Type:application/json" -d '{"port":{"admin_state_up":true,"name":"port0","tenant_id":"harishupadhyay","network_id":"00000000-0000-0000-0000-000000000000","segment_id":"blah"}}' http://$host:$port/v2.0/ports | jq "."
 done
--- get_port.py
+++ get_port.py
@@ -4,18 +4,34 @@ import json
 
 host='localhost'
 port=9696
+import thread
 
-get_query_string = 'http://' + host + ':' + str(port) + '/v2.0/ports'
-print('get query string', get_query_string)
-all_ports = requests.get(get_query_string)
-ports_json = json.loads(all_ports.text)
-port_list = ports_json['ports']
-for x in port_list:
-    print '-'*20
-    # For list of ports , issue a delete request
-    print 'deleting ', x['id']
-    delete_port_query_string = 'http://' + host + ':' + str(port) +'/v2.0/ports/' + str(x['id'])
-    delete_request = requests.delete(delete_port_query_string)
-    print 'Delete response ->', delete_request.text
+def get_and_delete_port():
+    get_query_string = 'http://' + host + ':' + str(port) + '/v2.0/ports'
+    print('get query string', get_query_string)
+    all_ports = requests.get(get_query_string)
+    ports_json = json.loads(all_ports.text)
+    port_list = ports_json['ports']
+    all_port_ids = []
+    for x in port_list:
+        # For list of ports , issue a delete request
+        all_port_ids.append(x['id'])
+    i = 1
+    total_port_count = len(all_port_ids)
+    while i < total_port_count:
+        try:
+            thread.start_new_thread(delete_port, (all_port_ids[i:i+100],))
+            print('i',i)
+            i= i + 100
+        except Exception as e:
+            print('Unable to start thread', str(e))
 
-#pprint.pprint(all_ports.text)
+
+def delete_port(port_list):
+    for ports in port_list:
+        delete_port_query_string = 'http://' + host + ':' + str(port) +'/v2.0/ports/' + ports
+        delete_request = requests.delete(delete_port_query_string)
+
+
+if __name__ == '__main__':
+    get_and_delete_port()
